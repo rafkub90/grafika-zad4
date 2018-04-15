9db13843db66a6065b0c2109dc624eb7c00cacbd
DESKTOP-HUMKCOQ\rafku <rafkub90@gmail.com>
2018-04-15 21:23:53 +0200

    no message

---
+++ Assets/trigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class trigger : MonoBehaviour {
+
+    public Light lt;
+    public Color color0 = Color.red;
+    public Color color1 = Color.blue;
+
+    // Use this for initialization
+    void Start () {
+       
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        lt.color = color0;
+
+        Destroy(this.gameObject);
+
+    }
+}
